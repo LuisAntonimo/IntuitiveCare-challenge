909ab34f4eec7d93386e2635527a7e7d8e9878bd
Luís Antônio <luisantoniossouza@yahoo.com.br>
2021-07-28 19:06:50 -0300

    base GUI

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>TESTE</h1>
+  <v-app>
+    <section>
+      <Header />
+    </section>
+    <main>
+      <section>
+        <DataTable />
+      </section>
+    </main>
+  </v-app>
 </template>
+
+<script>
+import Header from './components/header/header.vue';
+import DataTable from './components/dataTable/datatable.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {};
+  },
+  components: {
+    Header,
+    DataTable,
+  }
+};
+</script>
+
+<style>
+  #app {
+    background-color: #f1f1f1;
+  }
+</style>
---
+++ client/src/components/dataTable/datatable.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-data-table class="ma-4 elevation-1">
+    
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  data() {
+    
+  },
+};
+</script>
